ae1ef9a8a4ce390991eaa8e9f42e2069bc6b2fc6
Alice test <kimmzes@gmail.com>
2023-06-22 14:19:50 +0300

    Initial commit

---
+++ src/main/java/ru/netology/maven/Main.java
@@ -0,0 +1,11 @@
+import ru.netology.maven.BonusService;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int expected =150;
+        long actual = service.calculate(5_000, true);
+        System.out.println(expected + "== ? ==" + actual);
+    }
+}
