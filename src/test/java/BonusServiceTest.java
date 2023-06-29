d9f471ac8c9e28a2f345d64aa9859296adb6443a
OlesyaKhlybova <133571945+OlesyaKhlybova@users.noreply.github.com>
2023-06-29 14:40:25 +0300

    Update BonusServiceTest.java

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -41,7 +41,7 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000;
+        long amount = 100_000;
         boolean registered = false;
         long expected = 500;
 
@@ -57,9 +57,9 @@ public class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1_000_000;
+        long amount = 1_000;
         boolean registered = false;
-        long expected = 500;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
